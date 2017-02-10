54dde12e7d1350ad660d0153f1cffa169ef1e9a6
Jinghao Ma <aizensousuke724@yahoo.com>
2017-02-10 22:18:43 +0800

    Make up the idea

---
+++ yanghui_triangles.py
@@ -0,0 +1,14 @@
+def triangles():
+    L = [1]
+    while True:
+        yield L
+        L.append(0)
+        L = [L[i - 1] + L[i] for i in range(len(L))]
+n = 0
+for t in triangles():
+    print(t)
+    n = n + 1
+    if n == 10:
+        break
+        
+''' From http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014317799226173f45ce40636141b6abc8424e12b5fb27000'''
