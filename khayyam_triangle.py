133c0dfb93d236a34dae10a8098e903898b38825
Jinghao Ma <aizensousuke724@yahoo.com>
2017-02-13 23:44:28 +0800

    Got another way

--- khayyam_triangle.py
+++ khayyam_triangle.py
@@ -4,6 +4,8 @@ def triangles():
         yield L
         L.append(0)
         L = [L[i - 1] + L[i] for i in range(len(L))]
+        
+# Former codes were coded for last codes
 n = 0
 for t in triangles():
     print(t)
@@ -12,3 +14,25 @@ for t in triangles():
         break
         
 ''' From http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014317799226173f45ce40636141b6abc8424e12b5fb27000'''
+
+# Now, the way is straight with argument
+def make_next_row(row):
+    result = []
+    prev = 0
+    for i in row:
+        result.append(i + prev)
+        prev = i
+    result.append(prev)
+    return result
+
+def triangle(n):
+    result = []
+    current = [1]
+    for unused in range(0, n):
+        result.append(current)
+        current = make_next_row(current)
+
+    return result
+
+print(triangle(6))
+#>>> [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]
